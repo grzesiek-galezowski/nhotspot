f05c034f8704ad4bc709b4b7d9d40fbd19398197
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-05-27 23:45:13 +0200

    Moved the code from the TrainingExamples repository

---
+++ src/GitAttempt2/ApplicationLogic/Change.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ApplicationLogic
+{
+    public sealed class Change
+    {
+        public string Path { get; }
+        public DateTimeOffset ChangeDate { get; }
+        public string Text { get; }
+        public double Complexity { get; }
+
+        public Change(string path, string text, double complexity, DateTimeOffset changeDate)
+        {
+            Path = path;
+            ChangeDate = changeDate;
+            Text = text;
+            Complexity = complexity;
+            TextWithoutWhitespaces = Regex.Replace(Text, @"\s+", "");
+        }
+
+        public string TextWithoutWhitespaces { get; }
+    }
+}
+    
