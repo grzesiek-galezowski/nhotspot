3437af64a9f5f68a77c7e525143a291f4e23bb4a
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-06-14 17:11:51 +0200

    Added unfolding to file tree

--- src/GitAttempt2/ResultRendering/HtmlTemplateCustomCode.cs
+++ src/GitAttempt2/ResultRendering/HtmlTemplateCustomCode.cs
@@ -13,20 +13,38 @@ namespace ResultRendering
 
   public class PackageListView
   {
+    public static string GetJavaScript() => 
+      @"var toggler = document.getElementsByClassName(""caret"");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener(""click"", function() {
+    this.parentElement.querySelector("".nested"").classList.toggle(""active"");
+    this.classList.toggle(""caret-down"");
+  });
+}";
+
     public static string RenderFrom(PackageTreeNodeViewModel packageTree)
+    {
+      return Render2(packageTree, "id=\"myUL\"");
+    }
+
+    private static string Render2(PackageTreeNodeViewModel packageTree, string ulAttribute)
     {
       string result = string.Empty;
 
       if (packageTree.Children.Any())
       {
-        result += "<ul style=\"list-style-type:none\" >";
+        result += $"<ul {ulAttribute}>";
         foreach (var childPackage in packageTree.Children.OrderByDescending(c => c.HotSpotRating))
         {
           result += "<li>";
-          result += "<span>" + childPackage.Name + " (" + childPackage.HotSpotRating + ")" + "</span>";
-          result += RenderFrom(childPackage);
+          var spanClass = childPackage.Children.Any() ? "class=\"caret\"" : string.Empty;
+          result += $"<span {spanClass}>" + childPackage.Name + " (" + childPackage.HotSpotRating + ")" + "</span>";
+          result += Render2(childPackage, "class=\"nested\"");
           result += "</li>";
         }
+
         result += "</ul>";
       }
 
