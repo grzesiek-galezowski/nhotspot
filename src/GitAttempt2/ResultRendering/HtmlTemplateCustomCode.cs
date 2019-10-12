01bab83af5c800ba9fa5cb4c2473c0b0ffea1824
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-10-12 09:43:22 +0200

    Update HtmlTemplateCustomCode.cs

--- src/GitAttempt2/ResultRendering/HtmlTemplateCustomCode.cs
+++ src/GitAttempt2/ResultRendering/HtmlTemplateCustomCode.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace ResultRendering
 {
@@ -11,9 +12,68 @@ namespace ResultRendering
 
   }
 
+  public interface IHtmlContent
+  {
+    string ToString();
+    string Render(int nesting);
+  }
+
+  public class HtmlTag : IHtmlContent
+  {
+    public override string ToString()
+    {
+      return Render(0);
+    }
+
+    public string Render(int nesting)
+    {
+      //bug add nesting support
+      return nesting.Spaces() + $"<{_tagName}>" + Environment.NewLine + 
+             string.Join(" ", _children.Select(i => i.Render(nesting + 1) + Environment.NewLine)) 
+             + nesting.Spaces() + $"</{_tagName}>";
+    }
+
+    private readonly string _tagName;
+    private readonly IHtmlContent[] _children;
+
+    public HtmlTag(string tagName, IHtmlContent[] children)
+    {
+      _tagName = tagName;
+      _children = children;
+    }
+  }
+
+  public static class SpacesExtensions
+  {
+    public static string Spaces(this int count)
+    {
+      return new string(Enumerable.Repeat(' ', count).ToArray());
+    }
+  }
+
+  public class HtmlString : IHtmlContent
+  {
+    public override string ToString()
+    {
+      return _tagContent;
+    }
+
+    public string Render(int nesting)
+    {
+      return nesting.Spaces() + _tagContent;
+    }
+
+    private readonly string _tagContent;
+
+    public HtmlString(string tagContent)
+    {
+      _tagContent = tagContent;
+    }
+  }
+
   public class PackageListView
   {
-    public static string RenderFrom(PackageTreeNodeViewModel packageTree)
+    public static IHtmlContent RenderFrom(PackageTreeNodeViewModel packageTree)
     {
       if (packageTree.Children.Any())
       {
@@ -22,28 +82,31 @@ namespace ResultRendering
       }
       else
       {
-        return string.Empty;
+        return new HtmlString(string.Empty);
       }
     }
 
-    private static string RenderChildPackage(PackageTreeNodeViewModel childPackage)
+    private static IHtmlContent RenderChildPackage(PackageTreeNodeViewModel childPackage)
     {
       if (childPackage.Children.Any())
       {
         return
-          Tag("details",
-            Tag("summary", childPackage.Name + " (" + childPackage.HotSpotRating + ")"),
-            RenderFrom(childPackage));
+          Tag("details", Tag("summary", Text(childPackage.Name + " (" + childPackage.HotSpotRating + ")")), RenderFrom(childPackage));
       }
       else
       {
-        return Tag("span", childPackage.Name + " (" + childPackage.HotSpotRating + ")");
+        return Tag("span", Text(childPackage.Name + " (" + childPackage.HotSpotRating + ")"));
       }
     }
 
-    private static string Tag(string tagName, params string[] inside)
+    private static IHtmlContent Text(string text)
     {
-      return $"<{tagName}>" + string.Join(" ", inside) + $"</{tagName}>";
+      return new HtmlString(text);
+    }
+
+    private static IHtmlContent Tag(string tagName, params IHtmlContent[] children)
+    {
+      return new HtmlTag(tagName, children);
     }
   }
 
