55ddf6149870a5a1b5febf02f14078292aeb07ec
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2020-01-14 22:29:01 +0100

    build script

--- src/GitAttempt2/Build/Script.cs
+++ src/GitAttempt2/Build/Script.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Threading.Channels;
-using static AtmaFileSystem.AtmaFileSystemPaths;
+﻿using static AtmaFileSystem.AtmaFileSystemPaths;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
@@ -14,20 +10,48 @@ namespace Build
     {
       var repositoryRoot = AbsoluteDirectoryPath(Git.CurrentRepositoryPath());
       var slnPath = repositoryRoot + DirectoryName("src") + DirectoryName("GitAttempt2");
-      var consoleAppPath = slnPath + DirectoryName("GitAttempt2") + FileName("NHotSpot.Console.csproj");
+      var consoleAppPath = slnPath + DirectoryName("NHotSpot.Console");
+      var outputPath = repositoryRoot + DirectoryName("output");
+      const string version = "0.1.0";
+      
+      Target("clean", () =>
+      {
+        Run("rm", $"-r {outputPath}");
+      });
+      Target("build", () =>
+      {
+        Run("dotnet", 
+          Args(
+            $"build {consoleAppPath} ",
+            $"-p:VersionPrefix={version} ",
+            "-c Release ",
+            $"-o {outputPath} "
+            ));
+      });
+      Target("test", DependsOn("build"), () =>
+      {
+        Run("dotnet", $"test {slnPath}");
+      });
+      Target("pack", DependsOn("test"), () =>
+      {
+        Run("dotnet",
+          Args(
+            $"pack {consoleAppPath}", 
+            "--include-symbols", 
+            "-p:SymbolPackageFormat=snupkg", 
+            $"-p:VersionPrefix={version} ", 
+            "-c Release", 
+            $"-o {outputPath} "));
+      });
 
-      Target("clean", () => Run("rm", "-r ./output"));
-      Target("build", () => Run("dotnet", $"build {slnPath}"));
-      Target("test", DependsOn("build"), () => Run("dotnet", $"test {slnPath}"));
-      Target("pack", DependsOn("test"), () => Run("dotnet", 
-        $"pack {consoleAppPath} " + //bug how to pack everything?
-        "-p:VersionPrefix=1.0.3 " + 
-        "-o ./output "
-      ));
-      Target("publish", DependsOn("clean"), () => Run("dotnet", "publish -o ./output"));
       Target("default", DependsOn("pack"));
 
       RunTargetsAndExit(args);
+    }
+
+    private static string Args(params string[] strings)
+    {
+      return string.Join(' ', strings);
     }
   }
 }
