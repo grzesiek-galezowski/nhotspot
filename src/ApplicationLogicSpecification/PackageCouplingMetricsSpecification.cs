11185f38c976b161d6f46dbb3fb063ec5f36c32f
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2021-02-05 22:49:04 +0100

    Create PackageCouplingMetricsSpecification.cs

---
+++ src/ApplicationLogicSpecification/PackageCouplingMetricsSpecification.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using ApplicationLogicSpecification.Automation;
+using AtmaFileSystem;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ApplicationLogicSpecification
+{
+  public class PackageCouplingMetricsSpecification
+  {
+    [Test]
+    public void ShouldContainNoDataWhenNoCommitsDetected()
+    {
+      var analysisResult = new RepoAnalysisDriver().Analyze(_ => { });
+
+      analysisResult.PackageCouplingMetrics().Should().BeEmpty();
+    }
+
+    [Test] //bug finish the test
+    public void ShouldCorrectlyCalculateCouplingForASingleCommit()
+    {
+      var analysisResult = new RepoAnalysisDriver().Analyze(flow =>
+      {
+        flow.Commit(commit =>
+        {
+          commit.Dir("A1\\A2\\A3\\").File("A.cs").Added();
+          commit.Dir("A1\\A2\\A21\\A3").File("B.cs").Added();
+        });
+      });
+
+      var entries = analysisResult.PackageCouplingMetrics().ToList();
+      entries.Should().HaveCount(1);
+      
+      entries.ElementAt(0).CouplingCount.Should().Be(1);
+      entries.ElementAt(0).Left.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A3"));
+      entries.ElementAt(0).Right.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A21\\A3"));
+      entries.ElementAt(0).LongestCommonPathPrefix.Should().Be("ROOT\\A1\\A2");
+      entries.ElementAt(0).PercentageOfLeftCommits.Should().Be(100);
+      entries.ElementAt(0).PercentageOfRightCommits.Should().Be(100);
+      entries.ElementAt(0).PercentageOfTotalCommits.Should().Be(100);
+    }
+    
+    [Test]
+    public void ShouldCalculatePercentagesAcrossCommits()
+    {
+      var analysisResult = new RepoAnalysisDriver().Analyze(flow =>
+      {
+        flow.Commit(commit =>
+        {
+          commit.Dir("A1\\A2\\A3\\").File("A.cs").Added();
+          commit.Dir("A1\\A2\\A21\\A3").File("B.cs").Added();
+        });
+        flow.Commit(commit =>
+        {
+          commit.Dir("A1\\A2\\A3\\").File("A.cs").Modified();
+          commit.Dir("A1\\A2\\A21\\A3").File("B.cs").Modified();
+          commit.Dir("B1").File("B.cs").Added();
+        });
+        flow.Commit(commit =>
+        {
+          commit.Dir("A1\\A2\\A21\\A3").File("B.cs").Modified();
+          commit.Dir("C1").File("C.cs").Added();
+        });
+        flow.Commit(_ => { });
+      });
+
+      var entries = analysisResult.PackageCouplingMetrics().ToList().OrderBy(e => e.PercentageOfLeftCommits);
+      entries.Should().HaveCount(4);
+      entries.ElementAt(0).CouplingCount.Should().Be(1);
+      entries.ElementAt(0).Left.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A21\\A3"));
+      entries.ElementAt(0).Right.Should().Be(RelativeDirectoryPath.Value("ROOT\\C1"));
+      entries.ElementAt(0).PercentageOfLeftCommits.Should().Be(33);
+      entries.ElementAt(0).PercentageOfRightCommits.Should().Be(100);
+      entries.ElementAt(0).PercentageOfTotalCommits.Should().Be(25);
+      entries.ElementAt(1).CouplingCount.Should().Be(1);
+      entries.ElementAt(1).Left.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A21\\A3"));
+      entries.ElementAt(1).Right.Should().Be(RelativeDirectoryPath.Value("ROOT\\B1"));
+      entries.ElementAt(1).PercentageOfLeftCommits.Should().Be(33);
+      entries.ElementAt(1).PercentageOfRightCommits.Should().Be(100);
+      entries.ElementAt(1).PercentageOfTotalCommits.Should().Be(25);
+      entries.ElementAt(2).CouplingCount.Should().Be(1);
+      entries.ElementAt(2).Left.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A3"));
+      entries.ElementAt(2).Right.Should().Be(RelativeDirectoryPath.Value("ROOT\\B1"));
+      entries.ElementAt(2).PercentageOfLeftCommits.Should().Be(50);
+      entries.ElementAt(2).PercentageOfRightCommits.Should().Be(100);
+      entries.ElementAt(2).PercentageOfTotalCommits.Should().Be(25);
+      entries.ElementAt(3).CouplingCount.Should().Be(2);
+      entries.ElementAt(3).Left.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A3"));
+      entries.ElementAt(3).Right.Should().Be(RelativeDirectoryPath.Value("ROOT\\A1\\A2\\A21\\A3"));
+      entries.ElementAt(3).PercentageOfLeftCommits.Should().Be(100);
+      entries.ElementAt(3).PercentageOfRightCommits.Should().Be(66);
+      entries.ElementAt(3).PercentageOfTotalCommits.Should().Be(50);
+    }
+  }
+}
